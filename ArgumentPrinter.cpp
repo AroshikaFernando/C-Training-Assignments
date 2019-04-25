bf379feecec8ef188e6ab2af3db26fb6037bdad2
Mareena Fernando <Mareena.fer@gmail.com>
2019-04-25 17:14:44 +0530

    Update ArgumentPrinter.cpp

--- ArgumentPrinter.cpp
+++ ArgumentPrinter.cpp
@@ -1,19 +1,19 @@
-//
-// Created by mareenaf on 4/18/2019.
-//
 
-//#include <iostream>
-//#include <cstdio>
-//
-//using namespace std;
-//
-//int main(int argc , char** argv) {
-//    for (int i = 0; i < argc; ++i) {
-//
-//        // printf("Arg %d : %c\n" , i , argv[i]);
-//
-//        cout << "Arg" << i << " : " << argv[i] << "\n";
-//    }
-//    return 0;
-//}
+Created by mareenaf on 4/18/2019.
+
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc , char** argv) {
+   for (int i = 0; i < argc; ++i) {
+
+       // printf("Arg %d : %c\n" , i , argv[i]);
+
+       cout << "Arg" << i << " : " << argv[i] << "\n";
+   }
+   return 0;
+}
 
