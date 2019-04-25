9cd2c94aca77f9449c62052c3ce824fc4b370972
Mareena Fernando <Mareena.fer@gmail.com>
2019-04-25 17:15:52 +0530

    Update SimpleCalculator.cpp

--- SimpleCalculator.cpp
+++ SimpleCalculator.cpp
@@ -1,46 +1,46 @@
-//#include <iostream>
-//#include <cstdio>
-//#include <string>
-//
-//using namespace std;
-//
-//int main (){
-//
-//    float num1;
-//    float num2;
-//    char op;
-//
-//    cout << "\n Enter two numbers with the operation Ex:- x(operator)y -> x+y  and  If you want to quit enter any character  :  \n" ;
-//
-//    //|| ()
-//    while (cin >> num1 >> op >> num2){
-//        if((op >= 42 && op <= 47)){
-//            switch (op) {
-//                case '+':
-//                    cout << num1 + num2 << endl;
-//                    break;
-//                case '-':
-//                    cout << num1 - num2 << endl;
-//                    break;
-//                case '*':
-//                    cout << num1 * num2 << endl;
-//                    break;
-//                case '/':
-//                    cout << num1 / num2 << endl;
-//                    break;
-//            }
-//        }else{
-//            cout << "Invalid operator" << endl;
-//        }
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    return 0;
-//}
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main (){
+
+   float num1;
+   float num2;
+   char op;
+
+   cout << "\n Enter two numbers with the operation Ex:- x(operator)y -> x+y  and  If you want to quit enter any character  :  \n" ;
+
+   //|| ()
+   while (cin >> num1 >> op >> num2){
+       if((op >= 42 && op <= 47)){
+           switch (op) {
+               case '+':
+                   cout << num1 + num2 << endl;
+                   break;
+               case '-':
+                   cout << num1 - num2 << endl;
+                   break;
+               case '*':
+                   cout << num1 * num2 << endl;
+                   break;
+               case '/':
+                   cout << num1 / num2 << endl;
+                   break;
+           }
+       }else{
+           cout << "Invalid operator" << endl;
+       }
+   }
+
+
+
+
+
+
+
+
+
+   return 0;
+}
